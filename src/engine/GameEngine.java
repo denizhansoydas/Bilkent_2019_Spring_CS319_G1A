3dd7108ca145e55d9495b2be55ffc04dbb931adb
daswolfenblut <32513396+daswolfenblut@users.noreply.github.com>
2019-05-16 12:58:25 +0300

    Create GameEngine.java

---
+++ src/engine/GameEngine.java
@@ -0,0 +1,14 @@
+package engine;
+
+import javax.swing.SwingUtilities;
+
+public class GameEngine {
+	
+	public static void main(String[] args) {
+	      SwingUtilities.invokeLater(new Runnable() {
+	         public void run() {
+	        	gui.MainPanel.createTitleScreen();
+	         }
+	      });
+	   }
+}
